73cd9305da661f4de84ea545c0a3af91e8ace640
jun.zhua <jun.zhua@ctrip.com>
2018-08-03 20:15:57 +0800

    add test case

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import request from 'request'
 
-request('https://www.baidu.com', (error, response, body) => {
-  if (!error && response && response.statusCode === 200) {
-    console.log(body)
-  } else {
-    console.log('ERROR!')
-  }
-})
+export default () => {
+  request('https://www.baidu.com', (error, response, body) => {
+    if (!error && response && response.statusCode === 200) {
+      console.log(body)
+    } else {
+      console.log('ERROR!')
+    }
+  })
+}
